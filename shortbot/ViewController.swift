ed3097a5a9b17a475ef27e358fd6148dea2c9341
shivkanthb <shivkanthb@gmail.com>
2017-01-22 02:09:20 -0700

    basic table view

---
+++ shortbot/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  shortbot
+//
+//  Created by Shivkanth B on 1/22/17.
+//  Copyright © 2017 Shivkanth B. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
+
+
+
